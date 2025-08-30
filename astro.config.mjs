3f69bf29d70df4f6832edbbe01c91cb827c5ac0a
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-08-29 22:47:42 -0500

    pages links fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Camilo-845.github.io',
-  base: '/AcumulappWebPage',
-  integrations: [
-    alpinejs()
-  ],
+  site: "https://Camilo-845.github.io/AcumulappWebPage",
+  base: "/AcumulappWebPage",
+  integrations: [alpinejs()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
